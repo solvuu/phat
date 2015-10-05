c9626e3e03502063681fad0c4bc5acae8f73d4b2
Philippe Veber <philippe.veber@gmail.com>
2015-10-05 11:24:09 +0200

    test: random generators for paths and items

--- app/run_ounit_tests.ml
+++ app/run_ounit_tests.ml
@@ -2,6 +2,43 @@ open Core.Std
 open OUnit2
 open Phat
 
+let random_char () =
+  let a = Char.to_int 'a' in
+  let z = Char.to_int 'z' in
+  Char.of_int_exn (Random.int (z - a) + a)
+
+let random_name () =
+  String.init (Random.int 4 + 1) ~f:(fun _ -> random_char ())
+  |> name
+  |> ok_exn
+
+let rec random_rel_dir_item () =
+  match Random.bool (), Random.bool () with
+  | true, true -> Dir (random_name ())
+  | true, false -> (
+      let f p = Link (random_name (), p) in
+      match random_dir_path () with
+      | Abs_path p -> f p
+      | Rel_path p -> f p
+    )
+  | false, true -> Dot
+  | false, false -> Dotdot
+
+and random_dir_path () =
+  match Random.bool () with
+  | true -> Abs_path (random_abs_dir_path ())
+  | false -> Rel_path (random_rel_dir_path ())
+
+and random_abs_dir_path () =
+  concat root (random_rel_dir_path ())
+
+and random_rel_dir_path () =
+  let d = random_rel_dir_item () in
+  if Random.bool () then
+    Cons (d, random_rel_dir_path ())
+  else
+    Item d
+
 let not_names = [
   "foo/" ;
   "/foo" ;
