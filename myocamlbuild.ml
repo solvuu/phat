b8853dcc203a9553e496368edf8899fea11c6016
Ashish Agarwal <agarwal1975@gmail.com>
2016-02-16 12:01:03 -0500

    update wrt to latest solvuu_build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,29 +1,36 @@
 open Solvuu_build
 
 include Make(struct
+  let name = "phat"
+  let version = "dev"
+
   let info = Info.of_list [
     {
       Info.name = `Lib "pure";
       libs = [];
       pkgs = ["core_kernel"; "ppx_sexp_conv"; "ppx_here"];
+      build_if = [];
     };
 
     {
       Info.name = `Lib "async";
       libs = ["pure"];
       pkgs = ["async"];
+      build_if = [`Pkgs_installed];
     };
 
     {
       Info.name = `App "phat_tests";
       libs = ["async"];
       pkgs = ["oUnit"];
+      build_if = [`Pkgs_installed];
     };
 
     {
       Info.name = `App "phat";
       libs = ["async"];
       pkgs = [];
+      build_if = [`Pkgs_installed];
     };
   ]
 
