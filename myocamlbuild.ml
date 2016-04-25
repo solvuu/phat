1517e6433e75fe42ad8e1fbfaf4b6c2ae3cde7a4
Ashish Agarwal <agarwal1975@gmail.com>
2016-04-25 14:30:06 -0400

    myocamlbuild.ml: follow updates to solvuu_build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,43 +1,45 @@
-open Solvuu_build
+open Printf
+open Solvuu_build.Std
 
-include Make(struct
-  let name = "phat"
-  let version = "dev"
+let project_name = "phat"
+let version = "dev"
 
-  let info = Info.of_list [
-    {
-      Info.name = `Lib "pure";
-      libs = [];
-      pkgs = ["core_kernel"; "ppx_sexp_conv"; "ppx_here"];
-      build_if = [];
-    };
+let make_lib ?findlib_deps ?internal_deps ?build_if short_name : Item.t =
+  Item.lib (sprintf "%s_%s" project_name short_name)
+    ~pkg:(sprintf "%s.%s" project_name short_name)
+    ~dir:(sprintf "lib/%s" short_name)
+    ~pack_name:(sprintf "%s_%s" project_name short_name)
+    ?findlib_deps
+    ?internal_deps
+    ?build_if
 
-    {
-      Info.name = `Lib "async";
-      libs = ["pure"];
-      pkgs = ["async"];
-      build_if = [`Pkgs_installed];
-    };
+let pure = make_lib "pure"
+    ~findlib_deps:["core_kernel"; "ppx_sexp_conv"; "ppx_here"]
 
-    {
-      Info.name = `App "phat_tests";
-      libs = ["async"];
-      pkgs = ["oUnit"];
-      build_if = [`Pkgs_installed];
-    };
+let async = make_lib "async"
+    ~internal_deps:[pure]
+    ~findlib_deps:["async"]
+    ~build_if:[`Pkgs_installed]
 
-    {
-      Info.name = `App "phat";
-      libs = ["async"];
-      pkgs = [];
-      build_if = [`Pkgs_installed];
-    };
-  ]
+let phat_tests = Item.app "phat_tests"
+    ~file:"app/phat_tests.ml"
+    ~internal_deps:[async]
+    ~findlib_deps:["oUnit"]
+    ~build_if:[`Pkgs_installed]
 
-  let ocamlinit_postfix = [
-    "open Phat_async.Std";
-  ]
+let phat = Item.app "phat"
+    ~file:"app/phat.ml"
+    ~internal_deps:[async]
+    ~build_if:[`Pkgs_installed]
 
-end)
+let ocamlinit_postfix = [
+  "open Core.Std";
+  "open Async.Std";
+  "open Phat_async.Std";
+]
 
-let () = dispatch()
+let project = Project.make ~name:project_name ~version ~ocamlinit_postfix
+    [pure;async;phat_tests;phat]
+;;
+
+let () = Project.dispatch project
