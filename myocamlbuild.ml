e6e9a2e5224c53c43fb51a25167410260e32045b
Ashish Agarwal <agarwal1975@gmail.com>
2016-06-09 15:21:02 -0400

    myocamlbuild: update wrt latest solvuu_build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,36 +1,39 @@
 open Printf
 open Solvuu_build.Std
+open Solvuu_build.Util
 
 let project_name = "phat"
 let version = "dev"
 
-let make_lib ?findlib_deps ?internal_deps ?build_if short_name : Item.t =
-  Item.lib (sprintf "%s_%s" project_name short_name)
+let make_lib ?findlib_deps ?internal_deps ?ml_files short_name : Project.item =
+  Project.lib (sprintf "%s_%s" project_name short_name)
     ~pkg:(sprintf "%s.%s" project_name short_name)
     ~dir:(sprintf "lib/%s" short_name)
     ~pack_name:(sprintf "%s_%s" project_name short_name)
     ?findlib_deps
     ?internal_deps
-    ?build_if
+    ?ml_files
+
+let make_app ?internal_deps ?findlib_deps short_name : Project.item =
+  Project.app short_name
+    ~file:(sprintf "app/%s.ml" short_name)
+    ?internal_deps
+    ?findlib_deps
 
 let pure = make_lib "pure"
     ~findlib_deps:["core_kernel"; "ppx_sexp_conv"; "ppx_here"]
+    ~ml_files:(`Add ["about.ml"])
 
 let async = make_lib "async"
     ~internal_deps:[pure]
     ~findlib_deps:["async"]
-    ~build_if:[`Pkgs_installed]
 
-let phat_tests = Item.app "phat_tests"
-    ~file:"app/phat_tests.ml"
+let phat_tests = make_app "phat_tests"
     ~internal_deps:[async]
     ~findlib_deps:["oUnit"]
-    ~build_if:[`Pkgs_installed]
 
-let phat = Item.app "phat"
-    ~file:"app/phat.ml"
+let phat = make_app "phat"
     ~internal_deps:[async]
-    ~build_if:[`Pkgs_installed]
 
 let ocamlinit_postfix = [
   "open Core.Std";
@@ -38,8 +41,11 @@ let ocamlinit_postfix = [
   "open Phat_async.Std";
 ]
 
-let project = Project.make ~name:project_name ~version ~ocamlinit_postfix
-    [pure;async;phat_tests;phat]
-;;
+let optional_pkgs = ["async"; "lwt"]
 
-let () = Project.dispatch project
+let items =
+  [pure;async;phat_tests;phat] |>
+  List.filter ~f:(fun x -> Project.dep_opts_sat x optional_pkgs)
+
+;;
+let () = Project.solvuu1 ~project_name ~version ~ocamlinit_postfix items
